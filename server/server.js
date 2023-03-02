d83f9e2cac6a42d7c6d7673fb8fd5a9695d15eab
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2023-03-02 10:22:16 +0100

    setting up project structure and installing dependencies

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//middleware
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//database connection
+
+mongoose.connect(process.env.DB_LINK)
+    .then(() => console.log('database is connected'))
+    .catch((err) => console.log(err))
+
+//routes
+
+app.use('/user', userRouter);
+
+//run server on PORT
+const server = app.listen(PORT, () => {
+    console.log("listening on port " + PORT);
+});
